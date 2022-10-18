83a55be092ed7a19849855e41c569ec38d0e95ce
Dean <gao.dean@hotmail.com>
2022-10-19 07:45:28 +1100

    Change the path to freetype

--- config.mk
+++ config.mk
@@ -11,7 +11,7 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I. -I/usr/include -I/usr/include/freetype2 -I${X11INC}
+INCS = -I. -I/usr/local/include -I/usr/local/include/freetype2 -I${X11INC}
 LIBS = -L/usr/lib -lc -lm -L${X11LIB} -lXft -lfontconfig -lX11
 # OpenBSD (uncomment)
 #INCS = -I. -I${X11INC} -I${X11INC}/freetype2
